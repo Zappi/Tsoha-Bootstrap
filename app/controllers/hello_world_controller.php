90e879164be4b77241c33673d1d424c7a9e8b282
Jerry <jerry.tamlander@helsinki.fi>
2017-03-20 18:32:48 +0200

    Lisätty sivut

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,27 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-      echo 'Tämä on etusivu';
+      echo 'Tämä on etusivu. Jeejee';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
       View::make('helloworld.html');
+    }
+      
+    public static function etusivu() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+    
+    public static function kategorialistaus() {
+        View::make('suunnitelmat/kategorialistaussivu.html');
+    }  
+    
+    public static function reseptinlisays() {
+        View::make('suunnitelmat/reseptinlisays.html');
+    }  
+      
+    public static function reseptisivu() {
+        View::make('suunnitelmat/reseptisivu.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,21 @@
 <?php
 
   $routes->get('/', function() {
-    HelloWorldController::index();
+    HelloWorldController::etusivu();
   });
 
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
+  });
+
+   $routes->get('/kategoriat', function() {
+    HelloWorldController::kategorialistaus();
+  });
+
+ $routes->get('/lisaaresepti', function() {
+    HelloWorldController::reseptinlisays();
+  });
+
+ $routes->get('/reseptisivu', function() {
+    HelloWorldController::reseptisivu();
   });
